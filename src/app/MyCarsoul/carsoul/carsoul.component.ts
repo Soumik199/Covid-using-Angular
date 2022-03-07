53d8d47216d331c8e125b5e5041607f0fd3582f5
Soumik199 <Soumikroy519@gmail.com>
2022-03-07 19:38:13 +0530

    change in design of case component

--- src/app/MyCarsoul/carsoul/carsoul.component.ts
+++ src/app/MyCarsoul/carsoul/carsoul.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule, Router, NavigationEnd } from '@angular/router';
-
 @Component({
   selector: 'app-carsoul',
   templateUrl: './carsoul.component.html',
@@ -8,19 +6,6 @@ import { RouterModule, Router, NavigationEnd } from '@angular/router';
 })
 export class CarsoulComponent implements OnInit {
 
-  hideElement = false;
-  
-  constructor(private router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        if (event.url === '/case' || event.url === '/vaccine') {
-          this.hideElement = true;
-        }  else {
-          this.hideElement = false;
-        }
-      }
-    });
-  }
   ngOnInit(): void {
   }
 
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,20 +7,6 @@ import { Router,NavigationEnd } from '@angular/router';
 })
 export class FormComponent implements OnInit {
 
-  hideElement = false;
-  
-  constructor(private router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        if (event.url === '/case' || event.url === '/vaccine') {
-          this.hideElement = true;
-        }  else {
-          this.hideElement = false;
-        }
-      }
-    });
-  }
-
   ngOnInit(): void {
   }
 
